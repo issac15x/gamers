baf7e333b4817de08697f56eccbc08104667c82b
issac10x <ishakx49@gmail.com>
2021-07-25 20:46:20 +0100

    first test

---
+++ src/components/Landing.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+
+function Landing() {
+  
+  return (
+    <>
+      <nav>
+        <img src="https://image.flaticon.com/icons/png/128/3142/3142027.png" title="Flaticon" alt="" />
+        <div className="menu">
+          <h1>Login</h1>
+          <h1>Sign up</h1>
+        </div>
+      </nav>
+      <h1 id='yourway'>Your Way To Becom a Pro Gamer</h1>
+      <section>
+          
+          <div>
+            <img src="controller.svg" alt="" />
+            <h1>Get Started</h1>
+          </div>
+        
+        
+      </section>
+    </>
+    
+
+  );
+}
+
+export default Landing;
